f6ee79e43942cf541118f85c14bfc50b31ad2ff7
gowthaman <50874110+gowthamthaman6@users.noreply.github.com>
2019-08-23 09:36:45 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,23 @@
+name=input("")
+if name=="AI":
+    print(30)
+elif name=="EM":
+    print(28)
+elif name=="BA":
+    print(35)
+else:
+    print("Invalid airline")
+
+a=float(input())
+if a>=80 and a<=100:
+    print("A")
+elif a>=73 and a<=79:
+    print("B")
+elif a>=65 and a<=72:
+    print("C")
+elif a>=0 and a<=64:
+    print("D")
+else: 
+    print("Z")
+
+
